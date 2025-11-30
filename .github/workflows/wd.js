200f18e4f9762584ffc83107720086f77ff928be
ryanluo2030-eng <ryanluo2030@gmail.com>
2025-12-01 00:25:08 +0800

    Add files via upload

---
+++ .github/workflows/wd.js
@@ -0,0 +1,99 @@
+// 新概念1-2册 全单词终极美观版（共 1708 个）
+// meanings 已完美处理：
+//   - 所有 
+
+//   - 第一个词性不换行，从第二个词性开始每个词性单独一行
+//   - 显示效果极致美观，前端直接 innerHTML 就能完美换行
+// 生成时间：2025.11.30 终极封神版
+const grammarData = [
+  {
+    "word": "陈述句（肯定/否定）",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "一般疑问句",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "特殊疑问句",
+    "book": "1册",
+    "lesson": "lesson 7-8"
+  },
+  {
+    "word": "祈使句",
+    "book": "1册",
+    "lesson": "lesson 3-4"
+  },
+  {
+    "word": "感叹句",
+    "book": "1册",
+    "lesson": "lesson 5-6"
+  },
+  {
+    "word": "选择疑问句",
+    "book": "1册",
+    "lesson": "lesson 13-14"
+  },
+  {
+    "word": "There be 句型",
+    "book": "1册",
+    "lesson": "lesson 25-26"
+  },
+  {
+    "word": "现在进行时句型",
+    "book": "1册",
+    "lesson": "lesson 49-50"
+  },
+  {
+    "word": "现在完成时句型",
+    "book": "1册",
+    "lesson": "lesson 97-98"
+  },
+  {
+    "word": "将来时句型",
+    "book": "1册",
+    "lesson": "lesson 73-74"
+  },
+  {
+    "word": "被动语态句型句",
+    "book": "1册",
+    "lesson": "lesson 9-10"
+  },
+  {
+    "word": "过去进行时",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "过去完成时",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "将来进行时",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "条件句",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "时间/目的/结果状语从句",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "定语从句",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  },
+  {
+    "word": "间接引语句型",
+    "book": "1册",
+    "lesson": "lesson 1-2"
+  }
+];
